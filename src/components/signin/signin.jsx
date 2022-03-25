f74820833b095febb7b8d8aa2c404d0e8f8ed9b8
panjun <pndlm1979@naver.com>
2022-03-25 12:36:35 +0900

    signin, signup form

--- src/components/signin/signin.jsx
+++ src/components/signin/signin.jsx
@@ -6,6 +6,15 @@ const Signin = (props) => (
   <>
     <Header />
     <h1>signin</h1>
+    <form action="../welcome/welcome">
+      <input type="text" />
+      <input type="text" />
+      <button>signin</button>
+    </form>
+    <section>
+      <h5>signin with ohers</h5>
+      <button>kakao</button>
+    </section>
     <Footer />
   </>
 );
--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -6,6 +6,14 @@ const Signup = (props) => (
   <>
     <Header />
     <h1>signup</h1>
+    <form action="../signin/signin">
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <button>signup</button>
+    </form>
     <Footer />
   </>
 );
