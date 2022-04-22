d4aa7af071ec890f868eb3553ae0119550b935b5
Chana00 <63541383+Chana00@users.noreply.github.com>
2022-04-23 06:26:20 +0900

    error 수정

---
+++ ww-springboot/src/main/java/cap/workwalk/controller/HomeController.java
@@ -0,0 +1,9 @@
+package cap.workwalk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+}
